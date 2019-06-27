e10188217efc48433198809869268e69148427bc
Kevin Edmondson <kevinraye13@gmail.com>
2019-06-27 15:20:45 -0500

    tacos tab populated

--- src/components/menu/menu-tabs/Tacos.js
+++ src/components/menu/menu-tabs/Tacos.js
@@ -9,155 +9,281 @@ class Tacos extends React.Component {
           <div className='lunch--header pt3 pb3 bdb mb3'>
             <div className=''>
               <h2 className='LDHeader'>Tacos</h2>
-              <p />
+              <p>All Tacos Served All Day</p>
             </div>
           </div>
           <div className='items--container breakfast--container'>
             <ul>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  Huevos Rancheros
-                  <span className='price fw4 baskerville ttn'>
-                    <span className='i' /> (3.75) <br />
-                    <span className='i' /> W Bacon, Potatoes, or Sausage (4.75)
-                    <br />
-                    <span className='i'> W two of the above</span> (5.75)
-                  </span>
+                  Egg Taco
+                  <span className='price fw4 baskerville'>(1.35)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  Huevos a la Mexicana
-                  <span className='price fw4 baskerville'>(5.55)</span>
+                  Egg W/ Strip Bacon
+                  <span className='price fw4 baskerville'>(1.65)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  Papa a la Mexicana
-                  <span className='price fw4 baskerville'>(5.55)</span>
+                  Potato & Egg
+                  <span className='price fw4 baskerville'>(1.35)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  Machacado
-                  <span className='price fw4 baskerville'>(5.55)</span>
-                </h3>
-                <p className='item-description' />
-              </li>
-              <li className='pb3 pt1 measure'>
-                <h3 className='din ttu tracked f6'>
-                  Barbacoa
-                  <span className='price fw4 baskerville'>(5.75)</span>
-                </h3>
-                <p className='item-description'>Sat & Sun only</p>
-              </li>
-              <li className='pb3 pt1 measure'>
-                <h3 className='din ttu tracked f6'>
-                  Chicharrones
-                  <span className='price fw4 baskerville'>(5.50)</span>
-                </h3>
-                <p className='item-description' />
-              </li>
-              <li className='pb3 pt1 measure'>
-                <h3 className='din ttu tracked f6'>
-                  Chicharrones with Eggs
-                  <span className='price fw4 baskerville'>(6.25)</span>
-                </h3>
-                <p className='item-description' />
-              </li>
-              <li className='pb3 pt1 measure'>
-                <h3 className='din ttu tracked f6'>
-                  Chorizo con Huevo
-                  <span className='price fw4 baskerville'>(5.55)</span>
-                </h3>
-                <p className='item-description' />
-              </li>
-              <li className='pb3 pt1 measure'>
-                <h3 className='din ttu tracked f6'>
-                  Two Eggs
-                  <span className='price fw4 baskerville'>(2.50)</span>
-                </h3>
-                <p className='item-description' />
-              </li>
-              <li className='pb3 pt1 measure'>
-                <h3 className='din ttu tracked f6'>
-                  Mague's Special Plate
-                  <span className='price fw4 baskerville'>(5.50)</span>
-                </h3>
-                <p className='item-description'>
-                  Sausage, Chile, Tomato, Potato, Onions, Beans, No Egg
-                </p>
-              </li>
-              <li className='pb3 pt1 measure'>
-                <h3 className='din ttu tracked f6'>
-                  Papa Con Huevo
-                  <span className='price fw4 baskerville'>(5.50)</span>
-                </h3>
-                <p className='item-description' />
-                <p className='item-description' />
-              </li>
-              <li className='pb3 pt1 measure'>
-                <h3 className='din ttu tracked f6'>
-                  Chilaquiles
-                  <span className='price fw4 baskerville'>(5.50)</span>
+                  Chorizo & Egg
+                  <span className='price fw4 baskerville'>(1.35)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
                   Sausage & Egg
-                  <span className='price fw4 baskerville'>(5.50)</span>
+                  <span className='price fw4 baskerville'>(1.45)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Bacon & Egg
+                  <span className='price fw4 baskerville'>(1.45)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Cheese & Egg
+                  <span className='price fw4 baskerville'>(1.45)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Bean & Egg
+                  <span className='price fw4 baskerville'>(1.45)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Huevo a la Mexicana
+                  <span className='price fw4 baskerville'>(1.35)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Papa a la Mexicana
+                  <span className='price fw4 baskerville'>(1.35)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Carne a la Mexicana
+                  <span className='price fw4 baskerville'>(1.75)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Chorizo & Potato
+                  <span className='price fw4 baskerville'>(1.35)</span>
                 </h3>
                 <p className='item-description i' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  Bacon & Egg
-                  <span className='price fw4 baskerville'>(5.50)</span>
+                  Chicharron
+                  <span className='price fw4 baskerville'>(1.75)</span>
+                </h3>
+                <p className='item-description'>Soft pork skins</p>
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Quesadilla
+                  <span className='price fw4 baskerville'>(1.45)</span>
+                </h3>
+                <p className='item-description'>Corn & White Cheese</p>
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Bean
+                  <span className='price fw4 baskerville'>(1.35)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  One Pancake
-                  <span className='price fw4 baskerville'>(2)</span>
+                  Bean & Potato
+                  <span className='price fw4 baskerville'>(1.45)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  One Egg, 2 Silver Dollar Pancakes
-                  <span className='price fw4 baskerville'>(3)</span>
+                  Bean & Rice
+                  <span className='price fw4 baskerville'>(1.45)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  Two Eggs, Three Silver Dollar Pancakes
-                  <span className='price fw4 baskerville'>(4.25)</span>
+                  Bean & Chorizo
+                  <span className='price fw4 baskerville'>(1.35)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  Two Eggs, Bacon OR sausage, Papas, Toast
-                  <span className='price fw4 baskerville'>(5.50)</span>
+                  Randy W/ Strip
+                  <span className='price fw4 baskerville'>(2.10)</span>
+                </h3>
+                <p className='item-description'>papa, egg, bean, cheese</p>
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Ham & Egg
+                  <span className='price fw4 baskerville'>(1.85)</span>
                 </h3>
                 <p className='item-description' />
               </li>
               <li className='pb3 pt1 measure'>
                 <h3 className='din ttu tracked f6'>
-                  Silver Dollar Pancakes
+                  Bean & Sausage
+                  <span className='price fw4 baskerville'>(1.35)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Bean & Bacon
+                  <span className='price fw4 baskerville'>(1.55)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Bean & Cheese
+                  <span className='price fw4 baskerville'>(1.45)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Bean & Cheese & Bacon
+                  <span className='price fw4 baskerville'>(1.65)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Machacado
+                  <span className='price fw4 baskerville'>(1.45)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Picadillo
+                  <span className='price fw4 baskerville'>(1.45)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Carne Guisada
                   <span className='price fw4 baskerville'>(1.75)</span>
                 </h3>
                 <p className='item-description' />
               </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Carne de Puerco
+                  <span className='price fw4 baskerville'>(1.75)</span>
+                </h3>
+                <p className='item-description'>Chile Colorado only</p>
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Fajita
+                  <span className='price fw4 baskerville'>(2.25)</span>
+                  <p className='item-description'> Beef or Chicken </p>
+                  <span className='price fw4 baskerville'>
+                    W/ Guacamole or Cheese(2.50)
+                  </span>
+                  <br />
+                  <span className='price fw4 baskerville'>
+                    W/ Guacamole & Cheese(2.75)
+                  </span>
+                </h3>
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Guacamole
+                  <span className='price fw4 baskerville'>(1.60)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Lengua
+                  <span className='price fw4 baskerville'>(2.00)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Tripas Crispy or Soft
+                  <span className='price fw4 baskerville'>(3.10)</span>
+                  <br />
+                  <span className='price fw4 baskerville'>W/ Beans(3.35)</span>
+                </h3>
+                <p className='item-description' />
+              </li>
+              <li className='pb3 pt1 measure'>
+                <h3 className='din ttu tracked f6'>
+                  Barbacoa
+                  <span className='price fw4 baskerville'>(2.75)</span>
+                </h3>
+                <p className='item-description'>Sat & Sun only</p>
+              </li>
+              <div className='tacoBox'>
+                <p className='item-description'>
+                  (served with onion and cilantro on corn)
+                </p>
+                <li className='pb3 pt1 measure'>
+                  <h3 className='din ttu tracked f6'>
+                    Four Mini Tacos de Asada
+                    <span className='price fw4 baskerville'>(6.50)</span>
+                  </h3>
+                  <p className='item-description' />
+                </li>
+                <li className='pb3 pt1 measure'>
+                  <h3 className='din ttu tracked f6'>
+                    Four Mini Tacos al Pastor
+                    <span className='price fw4 baskerville'>(6.50)</span>
+                  </h3>
+                  <p className='item-description' />
+                </li>
+              </div>
             </ul>
           </div>
-
+          <div className='tacoBox'>
+            <h3 className='din ttu tracked f6 special'>
+              Magues Special Taco
+              <span className='price fw4 baskerville'>(6.50)</span>
+            </h3>
+            <p className='item-description'>
+              Chile, Sausage, Tomato, Onion, Potato, Beans
+            </p>
+          </div>
           <div className='bottom' />
         </body>
       </div>
